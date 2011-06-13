1bf258cc0f52c877a9d890b980a3ec38fe24addf
David Laing <david@davidlaing.com>
2011-06-13 10:40:15 +0100

    display dropped file

--- websockets/dragndrop.js
+++ websockets/dragndrop.js
@@ -1,20 +1,32 @@
-var stopPropagation = function(event){
-  event.stopPropagation();
-  event.preventDefault();
-}
 
-document.addEventListener('dragenter', stopPropagation, false);
-document.addEventListener('dragexit', stopPropagation, false);
-document.addEventListener('dragover', stopPropagation, false);
-document.addEventListener('drop', function (event) {
-  console.log('I got a drop event');
-  // your code here
-  // files available as event.dataTransfer.files
-});
+    var stopPropagation = function(event){
+      event.stopPropagation();
+      event.preventDefault();
+    }
 
-//var fileReader = new FileReader();
-//fileReader.onloadend = function(event){
-//  // Your code
-//  // (file data is in event.target.result)
-//}
-//fileReader.readAsDataURL(file);
+    document.addEventListener('dragenter', stopPropagation, false);
+    document.addEventListener('dragexit', stopPropagation, false);
+    document.addEventListener('dragover', stopPropagation, false);
+    document.addEventListener('drop', function (event) {
+        var file = event.dataTransfer.files[0];
+
+        console.log('I got a file dropped on me!', file);
+        var fileReader = new FileReader();
+
+        fileReader.onloadend = function(evt) {
+            if (!evt.target.error) {
+                //Base64 data
+                document.getElementById('droppedImage').src = evt.target.result;
+            }
+        }
+        fileReader.readAsDataURL(file);
+
+    });
+
+    //var fileReader = new FileReader();
+    //fileReader.onloadend = function(event){
+    //  // Your code
+    //  // (file data is in event.target.result)
+    //}
+    //fileReader.readAsDataURL(file);
+

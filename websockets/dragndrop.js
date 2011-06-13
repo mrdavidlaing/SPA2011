1dbbfc78bff51fd238b26da32bb0ee621831dc25
David Laing <david@davidlaing.com>
2011-06-13 11:19:25 +0100

    Store images in localstorage

--- websockets/dragndrop.js
+++ websockets/dragndrop.js
@@ -1,32 +1,47 @@
-
-    var stopPropagation = function(event){
-      event.stopPropagation();
-      event.preventDefault();
+function resetEvents() {
+    var stopPropagation = function(event) {
+        event.stopPropagation();
+        event.preventDefault();
     }
 
     document.addEventListener('dragenter', stopPropagation, false);
     document.addEventListener('dragexit', stopPropagation, false);
     document.addEventListener('dragover', stopPropagation, false);
+}
+
+function displayImages() {
+    $('#savedImages').empty();
+    var storedImages = JSON.parse(localStorage.getItem("myImages") || "[]");
+    for (var idx in storedImages) {
+        var newImage = new Image();
+        newImage.src = storedImages[idx];
+        $(newImage).bind('load', function() {
+            $(this).appendTo('#savedImages')
+        });
+    }
+}
+
+$(document).ready(function() {
+    resetEvents();
+
     document.addEventListener('drop', function (event) {
         var file = event.dataTransfer.files[0];
 
         console.log('I got a file dropped on me!', file);
-        var fileReader = new FileReader();
 
+        var fileReader = new FileReader();
         fileReader.onloadend = function(evt) {
             if (!evt.target.error) {
-                //Base64 data
-                document.getElementById('droppedImage').src = evt.target.result;
+
+                var storedImages = JSON.parse(localStorage.getItem("myImages") || "[]");
+                storedImages.push(evt.target.result);
+                localStorage.setItem('myImages', JSON.stringify(storedImages));
+
+                displayImages();
             }
         }
         fileReader.readAsDataURL(file);
-
     });
 
-    //var fileReader = new FileReader();
-    //fileReader.onloadend = function(event){
-    //  // Your code
-    //  // (file data is in event.target.result)
-    //}
-    //fileReader.readAsDataURL(file);
-
+    displayImages();
+})
